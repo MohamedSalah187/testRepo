049a7ca170030101a882dbb5363283f5a96b8e8e
mohamed <mohamedalsafier@gmail.com>
2023-03-17 17:36:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to github");
+
+
+            Console.ReadKey();
+        }
+    }
+}
